7ea32dc8ca40c43c3d0bb2d3d7d54394b593e15a
Yael Harosh <yaelharosh01@gmail.com>
2023-03-16 13:54:49 -0400

    finish

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: 'John',
+    lastName: 'Henry'
+  });
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+
+    return (
+      <form>
+        <input type="text" name="firstName" onChange={handleChange} value={formData.firstName} />
+        <input type="text" name="lastName" onChange={handleChange} value={formData.lastName} />
+        <button type="submit">Submit</button>
+      </form>
+    );
   }
-
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
-
-  return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
-  );
 }
 
 export default Form;
